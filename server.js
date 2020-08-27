be70f35264fe951bb749591847c9b2ccf078b845
samwel Mwaura <smmuiruri@gmail.com>
2020-08-27 08:41:12 -0700

    add connecting to db code

--- server.js
+++ server.js
@@ -7,13 +7,25 @@ const config = require('_config.js')
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
+const { connected } = require('process');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-    if (err) console.log(err)
-});
+// let mongodb_url = 'mongodb://localhost:27017/';
+// let dbName = 'darkroom';
+// mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
+//     if (err) console.log(err)
+// });
+
+// connecting the database
+const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
+mongoose.connect(MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology:true},
+   (err) => {
+       if(err){
+           console.log(err)
+       }else {
+           console.log("connected to Database: ${MONGODB_URI}")
+       }
+   });
 
 // test if the database has connected successfully
 let db = mongoose.connection;
