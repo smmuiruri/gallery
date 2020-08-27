4ca51d729ea9e7fffbd589c1f674bb02da19987c
samwel Mwaura <smmuiruri@gmail.com>
2020-08-27 07:06:42 -0700

    modify config file

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://sam:sam@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://sam:sam@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://sam:sam@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://sam:sam@cluster0-shard-00-02.f4e6e.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://sam:sam@cluster0-shard-00-02.f4e6e.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://sam:sam@cluster0-shard-00-02.f4e6e.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
